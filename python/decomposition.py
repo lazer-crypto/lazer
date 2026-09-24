768bbf5c5850520b526b9fc755e1b2f71d75513d
amerigal <antoniomerino99@gmail.com>
2026-09-24 09:18:18 +0200

    decomposition.py: remove unused imports

--- python/decomposition.py
+++ python/decomposition.py
@@ -2,9 +2,6 @@ import sys
 sys.path.append('..')   # path to lazer module
 from lazer import *     # import lazer python module
 from lazer import _invmod
-import hashlib      # for SHAKE128
-import time
-from labrados import *
 
 def makeGvec(ring,base,dim):
     """ 
